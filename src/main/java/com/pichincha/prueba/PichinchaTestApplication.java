1238d2fed602dc3bf491d3478a6f1803239b8b89
eduar2 <car.eduar2@gmail.com>
2023-10-15 12:17:40 -0500

    Initial commit

---
+++ src/main/java/com/pichincha/prueba/PichinchaTestApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PichinchaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PichinchaTestApplication.class, args);
+	}
+
+}
